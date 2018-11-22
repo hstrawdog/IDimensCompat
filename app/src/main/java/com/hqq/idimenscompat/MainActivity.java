67813712ccfbfaadfe4f7170c86b7f636d6a7143
593979591@qq.com <zz789789>
2018-11-22 16:48:02 +0800

    Initial commit

---
+++ app/src/main/java/com/hqq/idimenscompat/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hqq.idimenscompat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
